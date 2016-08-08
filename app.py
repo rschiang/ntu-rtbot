4ce5d51b2390a12ed6087d457ae352909443749c
Poren Chiang <ren.chiang@gmail.com>
2016-08-09 00:06:15 +0800

    Migrate to rule-based approach

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import rules
 from bottle import Bottle, request, abort
 from datetime import datetime
 from telegram import Bot, Update, ChatAction
@@ -14,6 +15,10 @@ IMAGE_DIR = os.environ.get('RT_IMAGE_DIR')
 # Variables
 app = Bottle()
 bot = Bot(token=TELEGRAM_TOKEN)
+rules = [
+    rules.HelloRule(),
+    rules.FallbackRule(),
+]
 
 @app.post('/hooks/<token>')
 def telegram_hook(token):
@@ -23,56 +28,24 @@ def telegram_hook(token):
     update = Update.de_json(request.json)
     chat_id = update.message.chat.id
     user = update.message.from_user
-    response_dict = {
-        'method': 'sendMessage',
-        'chat_id': chat_id,
-    }
 
     # Filter out users
     if user.username not in USERS:
-        response_dict['text'] = '哈囉 {}，我是阿屜。'.format(user.first_name)
-        return response_dict
+        return {
+            'method': 'sendMessage',
+            'chat_id': chat_id,
+            'text': '哈囉 {}，我是阿屜。'.format(user.first_name),
+        }
 
     message = update.message.text
-    if message == '/start' or '安安' in message:
-        response_dict['text'] = '嗨 {}，這裡是阿屜。'.format(user.username)
-
-    elif '天氣' in message:
-        import requests
-        try:
-            weather = requests.get('http://weather.ntustudents.org/api').json()
-            response_dict['text'] = '現在學校的氣溫是 {temperature} 度，降雨強度 {rain} mm/h。'.format(**weather)
-        except:
-            response_dict['text'] = '暫時無法取得天氣資訊。'
-
-    elif '會辦' in message:
-        import glob
-        recent_file = max(glob.iglob(IMAGE_DIR + '/*.jpg'), default=None)
-        if recent_file:
-            bot.sendChatAction(chat_id=chat_id, action=ChatAction.UPLOAD_PHOTO)
-            with open(recent_file, 'rb') as f:
-                caption = datetime.fromtimestamp(os.path.getctime(recent_file)).strftime('%Y/%m/%d %H:%M:%S')
-                bot.sendPhoto(chat_id=chat_id, photo=f, caption=caption)
-            response_dict['text'] = '這是會辦最近的情況。'
-        else:
-            response_dict['text'] = '目前沒有會辦最近的紀錄。'
-
-    elif '現在' in message or '時間' in message:
-        now = datetime.now()
-        term = now.year - 1987
-        days = (now - datetime(now.year if now.month > 7 else now.year + 1, 8, 1, 0, 0)).days + 1
-        response_dict['text'] = '沒有問題，現在的時間是 {:%Y/%m/%d %H:%M}，也就是第 {} 屆的第 {} 天。'.format(now, term, days)
-
-    elif '你能' in message:
-        response_dict['text'] = '你可以問我現在的時間、學校的天氣、或是會辦的狀況。'
-
-    elif 'BOT' in message.lower():
-        response_dict['text'] = '可是他們會打模糊仗！(〒皿〒)'
-
-    else:
-        response_dict['text'] = '什麼什麼？你在說什麼？\n如果不知道要問些什麼，可以問我「你能幫上什麼忙？」'
-
-    return response_dict
+    for rule in rules:
+        text = rule.match(bot, message)
+        if text:
+            return {
+                'method': 'sendMessage',
+                'chat_id': chat_id,
+                'text': text,
+            }
 
 def set_webhook():
     return bot.set_webhook('{}/hooks/{}'.format(BASE_URL, WEBHOOK_TOKEN))
--- rules.py
+++ rules.py
@@ -1,16 +1,47 @@
 import re
 
-class RuleBase(object):
-
+'''
+Base class for bot rules.
+'''
+class Rule(object):
+    '''
+    Check if a rule applies and `run()` the rule; return `None` otherwise.
+    '''
     def match(self, bot, message):
-        for expr in self.match_exprs():
+        for expr in self.match_expr():
             m = re.search(expr, message.text)
             if m:
                 return self.run(bot, message, **m.groupdict())
         return None
 
-    def match_exprs(self):
+    '''
+    Return a set of regular expression rules. In default implementation of
+    `match()`, the returning value will be used to determine whether the rule
+    applies, while extract `run()` arguments from named groups.
+    '''
+    def match_expr(self):
         raise NotImplementedError
 
+    '''
+    Executes the rule.
+    '''
     def run(self, bot, message, **kwargs):
         pass
+
+
+class HelloRule(Rule):
+    def match(self, bot, message):
+        for keyword in ['/start', '安安', '你好', '哈囉', '嗨']:
+            if keyword in message.text:
+                return self.run(bot, message)
+        return None
+
+    def run(self, bot, message, **kwargs):
+        return '嗨 {}，這裡是阿屜。'.format(message.from_user.username)
+
+class FallbackRule(Rule):
+    def match(self, bot, message):
+        return self.run(bot, message)
+
+    def run(self, bot, message, **kwargs):
+        return '什麼什麼？你在說什麼？\n如果不知道要問些什麼，可以問我「你能幫上什麼忙？」'
