8dd0ffc1ac380ef7f7ac8604791f54138bd886fb
Poren Chiang <ren.chiang@gmail.com>
2016-08-08 20:25:42 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import os
+from bottle import Bottle, request, abort
+from telegram import Bot, Update
+
+# Config
+TELEGRAM_TOKEN = os.environ.get('RT_TELEGRAM_TOKEN')
+WEBHOOK_TOKEN = os.environ.get('RT_WEBHOOK_TOKEN', 'RT')
+BASE_URL = os.environ.get('RT_BASE_URL')
+USERS = os.environ.get('RT_WHITELIST', '').split(',')
+
+# Variables
+app = Bottle()
+bot = Bot(token=TELEGRAM_TOKEN)
+
+@app.post('/hooks/<token>')
+def telegram_hook(token):
+    if token != WEBHOOK_TOKEN:
+        abort(401, 'Unauthorized')
+    update = Update.de_json(request.json)
+    return {
+        'method': 'sendMessage',
+        'chat_id': update.message.chat.id,
+        'text': '哈囉 {}，我是阿屜。'.format(update.message.from_user.username),
+    }
+
+def set_webhook():
+    return bot.set_webhook('{}/hooks/{}'.format(BASE_URL, WEBHOOK_TOKEN))
+
+def generate_token():
+    import base64
+    return base64.urlsafe_b64encode(os.urandom(18)).decode()
+
+if __name__ == '__main__':
+    set_webhook()
+    app.run()
