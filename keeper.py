867be26e9baceff731d67215a23a8fa5ff89ab60
Poren Chiang <ren.chiang@gmail.com>
2016-11-22 22:50:21 +0800

    Refactor to double as standalone script

--- keeper.py
+++ keeper.py
@@ -26,15 +26,7 @@ def send_photo(fp, chat_id):
         caption = datetime.fromtimestamp(os.path.getctime(fp)).strftime('%Y/%m/%d %H:%M:%S')
         bot.sendPhoto(chat_id=chat_id, photo=f, caption=caption)
 
-if __name__ == '__main__':
-    now = datetime.now()
-    pattern = IMAGE_DIR + now.strftime('/%Y-%m-%dT*.jpg')
-    recent_files = sorted(glob.iglob(pattern), reverse=True)
-
-    if len(recent_files) < 2:
-        sys.exit()  # It's the start of the day
-
-    file_a, file_b = recent_files[0], recent_files[1]
+def compare_files(file_a, file_b):
     delta_seconds = os.path.getctime(file_a) - os.path.getctime(file_b)
     if delta_seconds > 750:
         if delta_seconds < 1500:
@@ -44,11 +36,23 @@ if __name__ == '__main__':
 
     med_a, rms_a, stdev_a = histogram(file_a)
     med_b, rms_b, stdev_b = histogram(file_b)
-    if (rms_a - rms_b) > 20:
+    if abs(rms_a - rms_b) > 20:
+        is_occupying = (rms_a > rms_b)
+        the_file = (file_a if is_occupying else file_b)
+        the_message = '哈囉，這裡是阿屜。會辦似乎變得{}一些了。附上最近的狀況，還請多注意囉。'.format(
+                      '明亮' if is_occupying else '暗')
         for master in MASTERS:
-            send_photo(file_a, chat_id=master)
-            bot.sendMessage(chat_id=master, text='哈囉，這裡是阿屜。會辦似乎變明亮一些了。')
-    elif (rms_b - rms_a) > 20:
-        for master in MASTERS:
-            send_photo(file_b, chat_id=master)
-            bot.sendMessage(chat_id=master, text='哈囉，這裡是阿屜。會辦似乎變得暗一些了。附上最近的狀況，還請多注意囉。')
+            send_photo(the_file, chat_id=master)
+            bot.sendMessage(chat_id=master, text=the_message)
+
+if __name__ == '__main__':
+    now = datetime.now()
+    pattern = IMAGE_DIR + now.strftime('/%Y-%m-%dT*.jpg')
+    recent_files = sorted(glob.iglob(pattern), reverse=True)
+
+    is_iterating = (len(sys.argv) > 1 and sys.argv[1] == 'backtrace')
+    while len(recent_files) >= 2:
+        file_a, file_b = recent_files[0], recent_files[1]
+        compare_files(file_a, file_b)
+        if not is_iterating:
+            break
