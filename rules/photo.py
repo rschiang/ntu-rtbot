2106d6fd28dbf67f74e903672883b1320ec0c949
Poren Chiang <ren.chiang@gmail.com>
2016-12-06 17:34:26 +0800

    Fix scoping

--- rules/photo.py
+++ rules/photo.py
@@ -13,41 +13,39 @@ class PhotoRule(Rule):
     def run(self, bot, message, **kwargs):
         recent_file = max(glob.iglob(IMAGE_DIR + '/*.jpg'), default=None)
         if recent_file:
-            self.send_photo(bot, message.chat.id, recent_file)
+            send_photo(bot, message.chat.id, recent_file)
             return '這是會辦最近的情況。'
         return '目前沒有會辦最近的紀錄。'
 
-    def format_payload(time):
-        return 'photo:{:%Y-%m-%d %H:%M:%S}'.format(time)
-
-    def send_photo(self, bot, chat_id, photo_path):
-        bot.sendChatAction(chat_id=chat_id, action=ChatAction.UPLOAD_PHOTO)
-        with open(photo_path, 'rb') as f:
-            photo_time = datetime.fromtimestamp(os.path.getctime(photo_path))
-            keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                [
-                    InlineKeyboardButton('≪ 更早', callback_data=self.format_payload(photo_time - timedelta(minutes=5))),
-                    InlineKeyboardButton('< 較早', callback_data=self.format_payload(photo_time),
-                ],
-                [
-                    InlineKeyboardButton('更新', callback_data='photo:capture'),
-                ]
-            ])
-            caption = photo_time.strftime('%Y/%m/%d %H:%M:%S')
-            bot.sendPhoto(chat_id=chat_id, photo=f, caption=caption)
-
     def query_callback(self, bot, callback_query):
         namespace, _, payload = callback_query.data.partition(':')
-        if namespace != 'photo':
-            return
+        if namespace != 'photo': return
 
         try:
             cursor_time = datetime.strptime(payload, '%Y-%m-%d %H:%M:%S')
-        except ValueError:
-            return
+        except ValueError: return
 
         recent_files = sorted(glob.iglob(IMAGE_DIR + '/*.jpg'), reverse=True)
         for recent_file in recent_files:
             if os.path.getctime(recent_file) < cursor_time:
-                self.send_photo(bot, callback_query.chat_instance, recent_file)
+                send_photo(bot, callback_query.chat_instance, recent_file)
                 break
+
+def format_payload(time):
+    return 'photo:{:%Y-%m-%d %H:%M:%S}'.format(time)
+
+def send_photo(bot, chat_id, photo_path):
+    bot.sendChatAction(chat_id=chat_id, action=ChatAction.UPLOAD_PHOTO)
+    with open(photo_path, 'rb') as f:
+        photo_time = datetime.fromtimestamp(os.path.getctime(photo_path))
+        keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            [
+                InlineKeyboardButton('≪ 更早', callback_data=format_payload(photo_time - timedelta(minutes=5))),
+                InlineKeyboardButton('< 較早', callback_data=format_payload(photo_time),
+            ],
+            [
+                InlineKeyboardButton('更新', callback_data='photo:capture'),
+            ]
+        ])
+        caption = photo_time.strftime('%Y/%m/%d %H:%M:%S')
+        bot.sendPhoto(chat_id=chat_id, photo=f, caption=caption)
